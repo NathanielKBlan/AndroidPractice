686d3ed9ee1b28ef6d5f8fa18fdaaa056bdc0ea1
NathanielKBlan <nateblanquel@gmail.com>
2018-01-16 16:54:49 -0800

    Modified on 01/16/18

--- app/src/main/java/com/example/nathaniel_k_blanquel/drawingapp/Triangle.java
+++ app/src/main/java/com/example/nathaniel_k_blanquel/drawingapp/Triangle.java
@@ -30,7 +30,7 @@ public class Triangle{
     //Number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
     static float triangleCoords[] = { //In counter-clockwise order
-            0.0f, 0.622008459f, 0.0f, //top
+            0.0f, 0.622008459f, 0.5f, //top
             -0.5f, -0.311004243f, 0.0f, //bottom left
             0.5f, -0.311004243f, 0.0f //bottom right
     };
